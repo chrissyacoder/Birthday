96bf2471b8c3a0a189c813e44a6a8e2d1b152c19
Christie Pamphile <cpam2011@yahoo.com>
2021-09-17 03:47:59 -0400

    second commit

--- setup/src/List.js
+++ setup/src/List.js
@@ -9,7 +9,7 @@ return <article key={id} className="person">
   <img src={image}  alt={name}/>
   <div>
     <h4>{name}</h4>
-    <p>{age} years </p>
+    <p>{age} years old</p>
   </div>
 </article>
       })}
