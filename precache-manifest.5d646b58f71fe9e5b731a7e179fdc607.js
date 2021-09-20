bd04856f62a896eae8c704be1988a4200b0737ee
Christie Pamphile <cpam2011@yahoo.com>
2021-09-20 05:47:08 -0400

    Updates

---
+++ precache-manifest.5d646b58f71fe9e5b731a7e179fdc607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a5499a28069eff52be625649ed1271",
+    "url": "/Birthday/index.html"
+  },
+  {
+    "revision": "7573611816f6b0d4a4e8",
+    "url": "/Birthday/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/Birthday/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Birthday/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7573611816f6b0d4a4e8",
+    "url": "/Birthday/static/js/main.8c162026.chunk.js"
+  },
+  {
+    "revision": "c2760ca6236fb14d95d7",
+    "url": "/Birthday/static/js/runtime-main.b868b28c.js"
+  }
+]);
